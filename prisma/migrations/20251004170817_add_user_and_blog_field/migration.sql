7691037350e358035a924347a5ef12fb40419293
Sojib Sheikh <sojib02018@gmail.com>
2025-10-04 23:29:22 +0600

    database update

---
+++ prisma/migrations/20251004170817_add_user_and_blog_field/migration.sql
@@ -0,0 +1,84 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Profile` table. If the table is not empty, all the data it contains will be lost.
+  - Added the required column `updatedAt` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Made the column `name` on table `User` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- CreateEnum
+CREATE TYPE "UserStatus" AS ENUM ('ACTIVE', 'INACTIVE', 'BLOCK');
+
+-- DropForeignKey
+ALTER TABLE "public"."Post" DROP CONSTRAINT "Post_authorId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "public"."Profile" DROP CONSTRAINT "Profile_userId_fkey";
+
+-- AlterTable
+-- AlterTable
+ALTER TABLE "User" 
+ADD COLUMN "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN "phone" TEXT,
+ADD COLUMN "picture" TEXT,
+ADD COLUMN "status" "UserStatus" NOT NULL DEFAULT 'ACTIVE',
+ADD COLUMN "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ALTER COLUMN "name" SET NOT NULL;
+
+
+-- DropTable
+DROP TABLE "public"."Post";
+
+-- DropTable
+DROP TABLE "public"."Profile";
+
+-- CreateTable
+CREATE TABLE "Blog" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "thumbnail" TEXT,
+    "isFeatured" BOOLEAN NOT NULL DEFAULT false,
+    "tags" TEXT[],
+    "views" INTEGER NOT NULL DEFAULT 0,
+    "authorId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Blog_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "liveSiteUrl" TEXT NOT NULL,
+    "frontendGitUrl" TEXT,
+    "backendGitUrl" TEXT,
+    "thumbnail" TEXT,
+    "techStack" TEXT[],
+    "features" TEXT[],
+    "startDate" TIMESTAMP(3) NOT NULL,
+    "authorId" INTEGER NOT NULL,
+    "endDate" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Blog_slug_key" ON "Blog"("slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Project_slug_key" ON "Project"("slug");
+
+-- AddForeignKey
+ALTER TABLE "Blog" ADD CONSTRAINT "Blog_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Project" ADD CONSTRAINT "Project_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
